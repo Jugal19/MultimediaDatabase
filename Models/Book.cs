10d02d3055c350f98508cecadd98865f152f7c36
Jugal Patel <jugalpatel@hotmail.ca>
2020-12-04 20:03:37 -0500

    made comments on movie class, book context, and book class

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,21 +8,28 @@ namespace MultimediaDatabase.Models
 {
     public class Book
     {
-        //
+        //bookID was used to create the number of times the user makes a new data
         public int bookID { get; set; }
 
+        //readerName is used to see who made these comments
         public string readerName { get; set; }
 
+        //bookName is the title of the book
         public string bookName { get; set; }
 
+        //year is when the book was released
         public int year { get; set; }
 
+        //age is used for the user/or the book depending on how old it is
         public int age { get; set; }
 
+        //the person who made the book
         public string author { get; set; }
 
+        //short description of the book 
         public string description { get; set; }
 
+        //the genre of the book
         public string genre { get; set; }
     }
 }
--- Models/BookContext.cs
+++ Models/BookContext.cs
@@ -7,11 +7,15 @@ namespace MultimediaDatabase.Models
 {
     public class BookContext : DbContext
     {
+        //connects to the database with the list of books and movies
         public BookContext(DbContextOptions<BookContext> options) : base(options)
         {
 
         }
+        //this is the list where it accesses the books
         public DbSet<Book> Books { get; set; }
+
+        //this is the list where it accesses the movies
         public DbSet<Movie> Movies { get; set; }
 
 
